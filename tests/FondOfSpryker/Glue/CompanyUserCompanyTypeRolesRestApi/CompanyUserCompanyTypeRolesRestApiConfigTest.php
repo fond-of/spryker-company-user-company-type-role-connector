c5fc3ec53773f5d97a12ca7f9f69012df024d388
Jozsef Geng <gengjozsef86@gmail.com>
2020-06-17 18:51:37 +0200

    fix  CompanyUserCompanyTypeRolesRestApiConfigTest  unit Test

--- tests/FondOfSpryker/Glue/CompanyUserCompanyTypeRolesRestApi/CompanyUserCompanyTypeRolesRestApiConfigTest.php
+++ tests/FondOfSpryker/Glue/CompanyUserCompanyTypeRolesRestApi/CompanyUserCompanyTypeRolesRestApiConfigTest.php
@@ -4,7 +4,7 @@ namespace FondOfSpryker\Zed\CompanyUserCompanyTypeRolesRestApi;
 
 use Codeception\Test\Unit;
 use FondOfSpryker\Glue\CompanyUserCompanyTypeRolesRestApi\CompanyUserCompanyTypeRolesRestApiConfig;
-use org\bovigo\vfs\vfsStream;
+use FondOfSpryker\Shared\CompanyUserCompanyTypeRolesRestApi\CompanyUserCompanyTypeRolesRestApiConstants;
 
 class CompanyUserCompanyTypeRolesRestApiConfigTest extends Unit
 {
@@ -20,16 +20,26 @@ class CompanyUserCompanyTypeRolesRestApiConfigTest extends Unit
     {
         parent::_before();
 
-        $this->vfsStreamDirectory = vfsStream::setup('root', null, [
-            'config' => [
-                'Shared' => [
-                    'stores.php' => file_get_contents(codecept_data_dir('stores.php')),
-                    'config_default.php' => file_get_contents(codecept_data_dir('config_default.php')),
-                ],
-            ],
-        ]);
+        $this->companyUserCompanyTypeRolesRestApiConfig = new class() extends CompanyUserCompanyTypeRolesRestApiConfig {
 
-        $this->companyUserCompanyTypeRolesRestApiConfig = new CompanyUserCompanyTypeRolesRestApiConfig();
+            /**
+             * @param string $key
+             * @param mixed|null $default
+             *
+             * @return mixed
+             */
+            protected function get($key, $default = null)
+            {
+                if ($key === CompanyUserCompanyTypeRolesRestApiConstants::VALID_COMPANY_TYPE_COMPANY_ROLES_FOR_SEARCH_LIST) {
+                    return [
+                        'type_1' => ['role_1'],
+                        'type_2' => ['role_2'],
+                    ];
+                }
+
+                return parent::get($key, $default);
+            }
+        };
     }
 
     /**
