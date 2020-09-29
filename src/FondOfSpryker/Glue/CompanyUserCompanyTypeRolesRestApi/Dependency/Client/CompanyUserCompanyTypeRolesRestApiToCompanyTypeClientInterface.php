6a4e7d318984328ed68c626573c3d1034038e154
Jozsef Geng <gengjozsef86@gmail.com>
2020-09-29 09:54:43 +0200

    remove depracted code

--- src/FondOfSpryker/Glue/CompanyUserCompanyTypeRolesRestApi/Dependency/Client/CompanyUserCompanyTypeRolesRestApiToCompanyTypeClientInterface.php
+++ src/FondOfSpryker/Glue/CompanyUserCompanyTypeRolesRestApi/Dependency/Client/CompanyUserCompanyTypeRolesRestApiToCompanyTypeClientInterface.php
@@ -7,16 +7,6 @@ use Generated\Shared\Transfer\CompanyTypeTransfer;
 
 interface CompanyUserCompanyTypeRolesRestApiToCompanyTypeClientInterface
 {
-    /**
-     * @deprecated use findCompanyTypeById instead
-     *
-     * @param \Generated\Shared\Transfer\CompanyTypeTransfer $companyTypeTransfer
-     *
-     * @return \Generated\Shared\Transfer\CompanyTypeTransfer
-     */
-    public function getCompanyTypeById(
-        CompanyTypeTransfer $companyTypeTransfer
-    ): CompanyTypeTransfer;
 
     /**
      * @param \Generated\Shared\Transfer\CompanyTypeTransfer $companyTypeTransfer
